9946a744f0a22addc43b9be5dbf6f7b5402397fd
Sanjeev <sanjeevksk1990@gmail.com>
2023-01-25 11:36:28 +0530

    first commit

---
+++ src/test/java/com/spring/busreservation/domain/BusreservationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.busreservation.domain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusreservationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
